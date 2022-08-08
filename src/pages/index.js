bec07b96063ed3e9b7774c836f70cf0a7cb7de9d
Steven-UA <60966307+Steven-UA@users.noreply.github.com>
2022-08-08 17:27:53 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,10 +30,10 @@ const IndexPage = () => {
       <Hero
         maxWidth={'500px'}
         image={'/banner1.png'}
-        title={'Essentials for a cold winter'}
-        subtitle={'Discover Autumn Winter 2021'}
-        ctaText={'shop now'}
-        ctaAction={goToShop}
+        title={'Trouw Steven & Dries'}
+        subtitle={'12 november 2022'}
+        #ctaText={'shop now'}
+        #ctaAction={goToShop}
       />
 
       {/* Message Container */}
